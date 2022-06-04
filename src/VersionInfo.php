501217f8e3538fd285ab31a097017df6e479079f
YTBJero <nguyenhieucoder47@gmail.com>
2022-06-04 19:26:56 +0700

    Fix missing info in config.yml

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -6,7 +6,7 @@ namespace davidglitch04\EpicCustomAlerts;
 
 final class VersionInfo{
 
-	public const PLUGIN_VERSION = "0.0.1";
+	public const PLUGIN_VERSION = "0.0.2";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 }
